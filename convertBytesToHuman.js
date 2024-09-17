638e10098e34300c5dfaba62fb77453fc784562d
E-Okishev <106948826+E-Okishev@users.noreply.github.com>
2024-09-17 22:21:46 +0300

    dz1

---
+++ convertBytesToHuman.js
@@ -0,0 +1,33 @@
+/*
+ * В этом задании надо разработать функцию
+ * `convertBytesToHuman`. Эта функция  должна принимать
+ * аргумент `bytes` только числового типа.
+ * На выходе функция должна отдать
+ * человекопонятную строку, которая будет
+ * отражать размер файла. Примеры использования:
+ * `convertBytesToHuman(1024) === '1 KB';`
+ * `convertBytesToHuman(123123123) === '117.42 MB';`
+ * Необходимо предусмотреть защиту от
+ * передачи аргументов неправильного типа
+ * и класса (например, отрицательные числа)
+ */
+
+export default function convertBytesToHuman(bytes) {
+  if (typeof bytes !== 'number' || bytes < 0 || isNaN(bytes)) {
+    return false;
+  }
+
+  const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+  if (bytes === 0) {
+    return '0 B';
+  }
+  const i = Math.floor(Math.log(bytes) / Math.log(1024));
+  const value = (bytes / Math.pow(1024, i));
+  let formattedValue;
+  if (value % 1 === 0) {
+    formattedValue = value
+  } else {
+    formattedValue = value.toFixed(2);
+  }
+  return `${formattedValue} ${sizes[i]}`
+}
---
+++ pull_request_template.md
@@ -0,0 +1,10 @@
+# Домашнее задание 1: Функции и тесты
+
+Прошу @martinkomitsky, @haseprogram, @kgrechin или @ваш_назначенный_ментор проверить его.
+
+Что было сделано:
+
+1. настроил все окружение;
+2. добавил в проект .gitignore;
+3. написал тест на функцию `convertBytesToHuman`;
+4. написал решение для трех функций `convertBytesToHuman`, `correctSentence`, `nonUniqueElements`;
